975be22f4846f1a5fda2cee51934984a47c27c0f
guiquadros <guiquadros35@gmail.com>
2021-11-29 01:06:40 -0300

    draw menu + reorganize rotines

--- uninter-ap-estrutura-dados-ex2/Source.cpp
+++ uninter-ap-estrutura-dados-ex2/Source.cpp
@@ -18,40 +18,142 @@ struct UninterStudentNode
     UninterStudentNode* right = NULL;
 } *Root;
 
-void generate_students_records_for_BST();
+void draw_menu();
+int read_int_input();
+void clear_keyboard_buffer();
+void input_RU_search();
 void insert_student(const char* name, const char* email, int RU);
+void generate_students_records_for_BST();
 
 int main()
 {
     // chama rotina para gerar os dados dos alunos da arvore
     generate_students_records_for_BST();
 
-    printf("Hello World!\n");
+    int option = -1;
+
+    do
+    {
+        // desenha o menu
+        draw_menu();
+
+        // le a opcao que o usuario selecionou
+        option = read_int_input();
+
+        // limpa a tela apos o usuario selecionar a opcao para destacar a saida
+        system("cls");
+
+        // decide a acao com base na opcao selecionada
+        switch (option)
+        {
+            // procura por um aluno
+        case 1:
+            input_RU_search();
+            system("pause");
+            break;
+
+            // sai do programa
+        case 2:
+            printf("O programa sera encerrado!\n\n");
+            break;
+
+            // usuario digitou uma opacao invalida
+        default:
+            printf("OPCAO INVALIDA! Pressione uma tecla para reiniciar o menu.\n\n");
+            system("pause");
+            break;
+        }
+
+    } while (option != 2); // o menu e desenhado ate o usuario decidir fechar informando a opcao '2'
+
     system("pause");
 
     return 0;
 }
 
+void draw_menu()
+{
+    // limpa a tela
+    system("cls");
+
+    // opcoes do menu
+    printf("MENU:\n");
+    printf("1 - Procurar aluno pelo RU.\n");
+    printf("2 - Fechar o programa\n");
+    printf("\n");
+    printf("Selecione uma opcao: ");
+}
+
+int read_int_input()
+{
+    int value;
+
+    // le a opcao como um numero inteiro
+    scanf_s("%d", &value);
+
+    // limpa o buffer do teclado para evitar problemas na leitura do input
+    clear_keyboard_buffer();
+
+    return value;
+}
+
+void clear_keyboard_buffer()
+{
+    char c;
+
+    do
+    {
+        c = getchar();
+    } while (c != '\n' && c != EOF);
+}
+
+void input_RU_search()
+{
+    int RU_search = 0;
+    printf("Informe o RU do aluno a ser buscado: ");
+    RU_search = read_int_input();
+
+    // TODO: search for this RU
+}
+
+void insert_student(const char* name, const char* email, int RU)
+{
+    // aloca memoria para inserir um novo aluno na arvore
+    UninterStudentNode* new_student = (UninterStudentNode*)malloc(sizeof(UninterStudentNode));
+
+    // preenche o nodo com os dados do aluno
+    strcpy_s(new_student->name, name);
+    strcpy_s(new_student->email, email);
+    new_student->RU = RU;
+
+    // arvore esta vazia
+    if (Root == NULL)
+    {
+        Root = new_student;
+        return;
+    }
+}
+
 void generate_students_records_for_BST()
 {
     // 1 - root
-    insert_student("Guilherme Quadros da Silva", 
-        "guiquadros35@gmail.com", 
+    insert_student("Guilherme Quadros da Silva",
+        "guiquadros35@gmail.com",
         3282910);
 
     // 2
-    insert_student("Pafuncio da Silva", 
-        "pafuncio.silva@gmail.com", 
+    insert_student("Pafuncio da Silva",
+        "pafuncio.silva@gmail.com",
         1234567);
 
     // 3
-    insert_student("Neymar da Silva Jr.", 
-        "menino-ney@gmail.com", 
+    insert_student("Neymar da Silva Jr.",
+        "menino-ney@gmail.com",
         5462537);
 
     // 4
-    insert_student("Kleitin Souza", 
-        "kleitin-souza@yahoo.com.br", 
+    insert_student("Kleitin Souza",
+        "kleitin-souza@yahoo.com.br",
         2452745);
 
     // 5
@@ -114,7 +216,7 @@ void generate_students_records_for_BST()
         "petap@gmail.com",
         2345678);
 
-    // 18
+    // 17
     insert_student("Pita",
         "pitap@gmail.com",
         3465735);
@@ -133,22 +235,4 @@ void generate_students_records_for_BST()
     insert_student("Ryu",
         "da-hadouken@capcom.com",
         3457812);
-}
-
-void insert_student(const char* name, const char* email, int RU)
-{
-    // aloca memoria para inserir um novo aluno na arvore
-    UninterStudentNode* new_student = (UninterStudentNode*)malloc(sizeof(UninterStudentNode));
-
-    // preenche o nodo com os dados do aluno
-    strcpy_s(new_student->name, name);
-    strcpy_s(new_student->email, email);
-    new_student->RU = RU;
-
-    // arvore esta vazia
-    if (Root == NULL)
-    {
-        Root = new_student;
-        return;
-    }
 }
